873524e5635b919ef7d7615fa0bff108a3792e6f
starik061 <starik061@gmail.com>
2023-06-22 14:18:25 +0300

    add some components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { AddContactForm } from './AddContactForm/AddContactForm';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
   render() {
-    return <></>;
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <AddContactForm />
+      </>
+    );
   }
 }
