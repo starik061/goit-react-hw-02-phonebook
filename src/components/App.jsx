a0666502f9cb6a100b2041ce1d6b894ce444edc0
starik061 <starik061@gmail.com>
2023-06-23 15:36:56 +0300

    some fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,13 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -45,7 +51,10 @@ export class App extends Component {
           name={this.state.name}
           number={this.state.number}
         />
-        <ContactsList contacts={this.state.contacts} />
+        <ContactsList
+          contacts={this.state.contacts}
+          onFilterChange={this.updateInputState}
+        />
       </>
     );
   }
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -5,6 +5,15 @@ export class ContactsList extends Component {
     return (
       <>
         <h2>Contacts</h2>
+        <label>
+          Find contacts by name
+          <input
+            type="text"
+            value={this.props.filter}
+            name="filter"
+            onChange={this.props.onFilterChange}
+          />
+        </label>
         <ul>
           {this.props.contacts.map(contact => {
             return (
